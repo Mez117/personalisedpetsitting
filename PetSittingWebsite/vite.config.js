71af50cdf334618a1614e987fb0b6bb2b8905de6
James Holton <mezatron@Jamess-Laptop.local>
2024-10-31 16:03:52 +1100

    website pages

--- PetSittingWebsite/vite.config.js
+++ PetSittingWebsite/vite.config.js
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/viewpreview/",
   plugins: [
     vue({
       template: { transformAssetUrls }
