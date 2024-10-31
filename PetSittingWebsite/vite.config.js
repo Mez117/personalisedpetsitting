974c4ea57eeedead863fb7a62fa64d14f3067bf2
James <jamesholton2511@gmail.com>
2024-11-01 00:12:10 +1100

    please work

--- PetSittingWebsite/vite.config.js
+++ PetSittingWebsite/vite.config.js
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/PetSitting/" : "/",
+  base: "/PetSitting/",
 
   plugins: [
     vue({
